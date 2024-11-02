57574bbf9d4799b84c8c1e959ed6d562dc6ce620
trantu0711 <dh52107408@student.stu.edu.vn>
2024-11-02 09:10:37 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b) {
+return a+b;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	int a,b;
+	cout <<"nhap a";
+	cin >>a;
+	cout <<"nhap b";
+	cin >>b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 
 	system("pause");
