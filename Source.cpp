da817fbdc77ac970b5b85dbcffcff1a1bd7048ea
Khiem2511 <dh52104569@student.stu.edu.vn>
2024-11-09 09:05:10 +0700

    demo ssh

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 int tong(int a, int b) {
@@ -16,6 +16,35 @@ int main()
 	cin >>b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 
+    int a;
+
+    cout << "Nhập một số nguyên dương: ";
+    cin >> a;
+
+    if (isPerfectNumber(num)) {
+        cout << a << " là số hoàn thiện." << endl;
+    }
+    else {
+        cout << a << " không phải là số hoàn thiện." << endl;
+    }
+
 	system("pause");
 	return 0;
+}
+int sohoanthien(int a) {
+    int sum = 1;  
+    for (int i = 2; i * i <= n; ++i) {
+        if (a % i == 0) {
+            sum += i;
+            if (i != a / i) {
+                sum += a / i;
+            }
+        }
+    }
+    return sum;
+}
+
+bool ktraSohoanthien(int a) {
+    if (a <= 1) return false;  
+    return sumOfDivisors(n) == a;
 }
