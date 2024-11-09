ce9f04dd10fdb435953db5392fd31ccaf32a9257
trantu0711 <dh52107408@student.stu.edu.vn>
2024-11-09 08:19:39 +0700

    update kt snt

--- Source.cpp
+++ Source.cpp
@@ -15,7 +15,21 @@ int main()
 	cout <<"nhap b";
 	cin >>b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	if (KTSNT(a) == true)
+		cout << a << " la so nguyen to!";
+	else
+		cout << a << " khong la so nguyen to!";
 
 	system("pause");
 	return 0;
+}
+
+bool KTSNT(int a)
+{
+	if (a < 2)
+		return false;
+	for (int i = 2; i <= a / 2; i++)
+		if (a % i == 0)
+			return false;
+	return true;
 }
